4dc849f7aca2b1db1d8eae0f4a80e9b0bf652eb0
34j <55338215+34j@users.noreply.github.com>
2022-10-15 19:23:54 +0900

    fix(code): Add additionalOptions in EasyOptionsBui

--- src/packageRunner/easyOptionsBuilder.ts
+++ src/packageRunner/easyOptionsBuilder.ts
@@ -6,6 +6,7 @@ export class EasyOptionsBuilder extends OptionsBuilderBase {
   private _flags: string[];
   private _parameters: string[];
   private _listParameters: string[];
+  private _additionalOptions: string[];
   protected get flags(): string[] {
     return this._flags;
   }
@@ -15,24 +16,30 @@ export class EasyOptionsBuilder extends OptionsBuilderBase {
   protected get listParameters(): string[] {
     return this._listParameters;
   }
+  protected get additionalOptions(): string[] {
+    return this._additionalOptions;
+  }
 
   constructor(
     packageInfo: PackageInfo,
     flags: string[],
     parameters: string[],
-    listParameters: string[]
+    listParameters: string[],
+    additionalOptions?: string[]
   ) {
     super(packageInfo);
     this._flags = flags;
     this._parameters = parameters;
     this._listParameters = listParameters;
+    this._additionalOptions = additionalOptions || [];
   }
 
   public async build(uris: vscode.Uri[]): Promise<string[]> {
     return (await super.build(uris)).concat(
       this.optionsBuilderHelper.buildFlags(this._flags),
       this.optionsBuilderHelper.buildParameters(this._parameters),
-      this.optionsBuilderHelper.buildListParameters(this._listParameters)
+      this.optionsBuilderHelper.buildListParameters(this._listParameters),
+      this._additionalOptions
     );
   }
 }
